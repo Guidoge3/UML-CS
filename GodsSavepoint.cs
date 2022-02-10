151973c5912fff071e2efbe158e139fddadf1247
Guidoge3 <70884054+Guidoge3@users.noreply.github.com>
2022-02-10 15:53:07 +0100

    kijk vader jesse alstu heb t af :D

---
+++ GodsSavepoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database_Gods
+{
+    class GodsSavepoint
+    {
+        public string name;
+        public GodsSavepoint(string theName)
+        {
+            name = theName;
+        }
+        public string AddName()
+        {
+            return name;
+        }
+    }
+}
